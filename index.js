ebd3799c56dba704465a44a5708e357ebe2ec4a9
henryhangit <henryhanying@qq.com>
2021-05-26 11:16:44 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Run `node index.js` in the terminal
 
 console.log(`Hello Node v${process.versions.node}!`);
+
+let c = Add(10.2, 20.4);
+console.log(c);
+
+function Add(a, b) {
+  return a + b;
+}
